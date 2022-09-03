abe8a590d17fa3a0e76d2004826d3901b73b6c4c
Sachith <sachithlakmal@icloud.com>
2022-09-03 12:41:27 +0530

    bootstrap

---
+++ src/test/java/com/vetstoria/appointments/VetAppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetstoria.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetAppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
